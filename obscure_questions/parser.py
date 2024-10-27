0caadfc4cf0eb0fd284034efdd684b4041a1160f
kaito1122 <119906364+kaito1122@users.noreply.github.com>
2024-10-27 11:50:49 -0400

    fix

--- obscure_questions/parser.py
+++ obscure_questions/parser.py
@@ -6,6 +6,7 @@ def parser(text: str):
     split by space, parse all the stop words, and remove punctuation
     """
     terms = text.split()
+    terms = [t.lower() for t in terms]
 
     nltk.download('stopwords')
     parsed = [t for t in terms if t not in stopwords.words('english')]
